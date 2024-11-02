9280e86435134dd4c7ca298f17446b8c6abd192e
Denis Sherstnev <88676144+sherstnew@users.noreply.github.com>
2024-11-02 12:44:47 +0300

    ws fix

--- scores.js
+++ scores.js
@@ -31,7 +31,7 @@ const timeout_b_checkbox = document.querySelector('#timeout_b');
 
 const sets_scores = document.querySelectorAll(".set_score");
 
-const socket = new WebSocket("wss://kasik.skfxteam.ru:8999");
+const socket = new WebSocket("wss://score-yigf.onrender.com");
 
 socket.onopen = () => {
     const send_scores = () => {
