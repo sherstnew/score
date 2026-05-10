a9eb019e9f2d89f7efacc6210f13c142f952e2e7
sherstnew <sherstnev.denis.v@gmail.com>
2026-05-10 22:18:56 +0300

    link changed to tablo

--- scores.js
+++ scores.js
@@ -33,7 +33,7 @@ const timeout_b_checkbox = document.querySelector('#timeout_b');
 
 const sets_scores = document.querySelectorAll(".set_score");
 
-const socket = new WebSocket("wss://score-yigf.onrender.com");
+const socket = new WebSocket("wss://tablo.sherstd.ru");
 let send_scores = () => {};
 
 const normalize_point_input = (input) => {
